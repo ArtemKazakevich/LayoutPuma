6bcf5e559c92059f9a822dda6f4036a08975d0f4
artem.kazakevich.91@gmail.com <artem.kazakevich.91@gmail.com>
2018-12-01 21:35:33 +0300

    sliced layout.

---
+++ build/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var addition = function addition(a, b) {
+  return a + b;
+};
+
+console.log('10 + 1: ', addition(10, 1));
+var army = makeArmy();
+army[0](); // 0
+
+army[5](); // 5
+//# sourceMappingURL=main.js.map
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const addition = (a, b) => a + b;
+
+console.log('10 + 1: ', addition(10, 1));
+
+var army = makeArmy();
+
+army[0](); // 0
+army[5](); // 5
